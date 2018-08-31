5335a68410b691b6728f076d9ef3e9f7c71ede04
Kassandra Perch <the@nodebotani.st>
2018-08-31 10:56:16 -0500

    Works

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Example App: IOpipe Integration for Pulumi
+
+## Step One: Set up Pulumi and IOpipe
+
+1. Make sure Pulumi is [installed](https://pulumi.io/quickstart/install.html) and set up with [your AWS access keys](https://pulumi.io/quickstart/aws/setup.html)
+1. Initialize your Pulumi project with `pulumi new`
+1. Install the Pulumi IOpipe plugin with `npm i --save @pulumi/iopipe`
+1. Set your IOpipe access token using `pulumi config set --secret iopipe:token <your token here>`
+
+## Step Two: Write your Lambda function using Pulumi
+
+1. Install dependencies: `npm i --save @pulumi/aws-cloud @pulumi/aws-serverless`
+1. Write your AWS Lambda function (Also seen in [index.js](./index.js):
+
+```javascript
+const cloud = require('@pulumi/cloud-aws')
+
+
+```
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-"use strict";
-const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
+const serverless = require("@pulumi/aws-serverless");
+const cloud = require("@pulumi/cloud-aws");
 
-// Create an AWS resource (S3 Bucket)
+// Load the Pulumi IO| integration package
+require("@pulumi/iopipe");
+
+// Create a public HTTP REST API endpoint (using AWS APIGateway)
+const endpoint = new cloud.API("hello");
+    
+// Serve a simple REST API on `GET /name` (using AWS Lambda)
+endpoint.get("/source", (req, res) => res.json({name: "AWS"}))
+    
+// Export the public URL for the HTTP service
+exports.url = endpoint.publish().url;
+
+// Create a bucket and a function to log new object uploads
 const bucket = new aws.s3.Bucket("my-bucket");
-
-// Export the DNS name of the bucket
-exports.bucketName = bucket.bucketDomainName;
+serverless.bucket.onPut("onNewObject", bucket, async (ev) => console.log(ev));
+exports.bucketName = bucket.bucket;
